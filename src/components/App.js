629630f4dc146018f19eff3f7a37f2f12006b7e8
Vasily93 <markovavasily@gmail.com>
2019-09-03 10:05:36 -0700

    created and action

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import AddContact from './phonebook/AddContact';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <AddContact />;
 }
-  
--- src/components/phonebook/AddContact.js
+++ src/components/phonebook/AddContact.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types';
 function AddContact({ handleSubmit, phone, name, address }) {
   return (
     <form onSubmit={handleSubmit}>
-      <input value={phone} placeholder='Phone'/>
       <input value={name} placeholder='Name'/>
+      <input value={phone} placeholder='Phone'/>
       <input value={address} placeholder='Address'/>
-      <buuton>Add Contact</buuton>
+      <button>Add Contact</button>
     </form>
   );
 }
