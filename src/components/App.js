597b90cc16a07397ef743b01441fc4ccce988f05
Vasily93 <markovavasily@gmail.com>
2019-09-05 15:18:44 -0700

    added authentication with auth0

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import AddContactContainer from '../containers/AddContactContainer';
+import { useAuth0 } from '../AuthProvider';
 
 export default function App() {
+  const { isAuthenticated, user, loading, auth0Client } = useAuth0();
+  const login = () => auth0Client.loginWithRedirect();
+  const logout = () => auth0Client.logout();
+
   return (
-    <> 
+    <>
+      {!isAuthenticated && <button onClick={login}>LOGIN</button>}
+      {isAuthenticated && <button onClick={logout}>LOGOUT</button>}
     <h1>PHONEBOOK</h1>
       <AddContactContainer />
     </>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './store';
+import { Provider } from 'react-redux';
+import AuthProvider from './AuthProvider';
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <AuthProvider
+    domain='vasily-markov1993.auth0.com'
+    client_id='Q48Z57tNLN2w3r935iciEeDcO6qzocbb'
+    redirect_uri='http://localhost:7890'
+  >
+    <Provider store={store} >
+      <App  />
+    </Provider>
+  </AuthProvider>,
   document.getElementById('root')
 );
